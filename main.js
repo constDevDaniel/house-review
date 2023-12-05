b511c0c80ba92058d0ccee6de7d98bdc79a94a87
dinorex01 <danielazoulay2010@gmail.com>
2023-12-05 16:49:20 -0300

    TCC completo

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ var swiper = new Swiper(".mySwiper", {
 	},
 	loop: true,
 });
+
+// JavaScript
+document.body.addEventListener("scroll", function () {
+	var scrollHeight = Number(document.documentElement.scrollHeight);
+	var scrollTop = Number(document.documentElement.scrollTop);
+	var clientHeight = Number(document.documentElement.clientHeight);
+	var scrolled = (scrollTop / (scrollHeight - clientHeight)) * 100;
+	console.log(scrolled);
+	console.log(scrolled);
+	if (scrolled > 10) {
+		document.getElementById("backTopButton").style.display = "block";
+	} else {
+		document.getElementById("backTopButton").style.display = "none";
+	}
+});
